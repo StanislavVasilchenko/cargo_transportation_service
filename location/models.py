175c919aa15ab354df00b5fc031bf3d1063eb0e2
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-26 21:33:19 +0300

    create Location model

--- location/models.py
+++ location/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=100, verbose_name="City")
+    state = models.CharField(max_length=100, verbose_name="State")
+    zipcode = models.CharField(max_length=100, verbose_name="Zipcode")
+    latitude = models.FloatField(verbose_name="Latitude")
+    longitude = models.FloatField(verbose_name="Longitude")
+
+    def __str__(self):
+        return f'{self.zipcode} - {self.city}'
+
+    class Meta:
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+        ordering = ['zipcode']
