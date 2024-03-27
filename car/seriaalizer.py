7439d5aed28eda72b548f8cdcd7a6183b4381b48
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-27 13:23:52 +0300

    add CarLiftingValidator

--- car/seriaalizer.py
+++ car/seriaalizer.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 from car.models import Car
-from car.validators import CarNumberValidator
+from car.validators import CarNumberValidator, CarLiftingValidator
 
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         exclude = ('current_location',)
-        validators = [CarNumberValidator(field='number'),]
+        validators = [CarNumberValidator(field='number'),
+                      CarLiftingValidator(field='lifting_capacity')]
