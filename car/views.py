5234caba8b897c80d8f0102bf2290272d2a47478
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-27 13:15:37 +0300

    add CarCreateAPIView

--- car/views.py
+++ car/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+
+from car.models import Car
+from car.seriaalizer import CarSerializer
+from location.models import Location
+
+
+class CarCreateAPIView(generics.CreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        location = Location.objects.all()[random.randint(1, Location.objects.all().count() - 1)]
+        serializer.save(current_location=location)
