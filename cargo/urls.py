5ef27d2d765b71f63e644705231c819115bea339
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-28 16:09:55 +0300

    add cargo-detail url

--- cargo/urls.py
+++ cargo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from cargo.apps import CargoConfig
-from cargo.views import CargoCreateAPIView, CargoListAPIView
+from cargo.views import CargoCreateAPIView, CargoListAPIView, CargoDetailAPIView
 
 app_name = CargoConfig.name
 
 urlpatterns = [
     path('create/', CargoCreateAPIView.as_view(), name='cargo-create'),
     path('list/', CargoListAPIView.as_view(), name='cargo-list'),
+    path('detail/<int:pk>/', CargoDetailAPIView.as_view(), name='cargo-detail'),
 ]
