89a08313567ab884222f15070ef876c761df4a81
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-28 17:29:15 +0300

    add cargo-delete

--- cargo/urls.py
+++ cargo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from cargo.apps import CargoConfig
-from cargo.views import CargoCreateAPIView, CargoListAPIView, CargoDetailAPIView, CargoUpdateAPIView
+from cargo.views import CargoCreateAPIView, CargoListAPIView, CargoDetailAPIView, CargoUpdateAPIView, CargoDeleteAPIView
 
 app_name = CargoConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('list/', CargoListAPIView.as_view(), name='cargo-list'),
     path('detail/<int:pk>/', CargoDetailAPIView.as_view(), name='cargo-detail'),
     path('update/<int:pk>/', CargoUpdateAPIView.as_view(), name='cargo-update'),
+    path('delete/<int:pk>/', CargoDeleteAPIView.as_view(), name='cargo-delete'),
 ]
