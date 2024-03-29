10169682a07e9bc1c8d172a109538e0ee4e84dc1
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-29 23:17:55 +0300

    ref fill_cars command

--- car/management/commands/fill_cars.py
+++ car/management/commands/fill_cars.py
@@ -8,16 +8,17 @@ from location.models import Location
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        cars = []
-        count = 1
-        location = Location.objects.all()
-        for i in range(20):
-            car = {
-                'number': str(1111 + count) + 'A',
-                'current_location': location[random.randint(0, len(location) - 1)],
-                'lifting_capacity': 200 + count
-            }
-            cars.append(Car.objects.create(**car))
-            count += 1
+        if not Car.objects.all():
+            cars = []
+            count = 1
+            location = Location.objects.all()
+            for i in range(20):
+                car = {
+                    'number': str(1111 + count) + 'A',
+                    'current_location': location[random.randint(0, len(location) - 1)],
+                    'lifting_capacity': 200 + count
+                }
+                cars.append(Car(**car))
+                count += 1
 
-        Car.objects.bulk_create(cars)
+            Car.objects.bulk_create(cars)
