dc0e0793de0b6e74a1743596fb26f90fd5011e85
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-29 23:18:22 +0300

    ref fill_locations command

--- location/management/commands/fill_locations.py
+++ location/management/commands/fill_locations.py
@@ -6,6 +6,7 @@ from location.services import read_csv_locations
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        locations = read_csv_locations()
-        data_for_db = [Location(**location) for location in locations]
-        Location.objects.bulk_create(data_for_db)
+        if not Location.objects.all():
+            locations = read_csv_locations()
+            data_for_db = [Location(**location) for location in locations]
+            Location.objects.bulk_create(data_for_db)
